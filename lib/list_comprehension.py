11f733011e5570e74368bcdcd71bee4c548ca8ec
Eric Maranga <ekirira22@gmail.com>
2024-03-03 03:07:46 +0300

    completed assignent

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_numbers = [even for even in num_list if even % 2 == 0]
+    return even_numbers
+
 
 def make_exclamation(sentence_list):
-    pass
+    exclaimed_list = [f"{list_item}!" for list_item in sentence_list]
+    return exclaimed_list
+
+
+print(make_exclamation(["Hello", "I'm doing great", "Python is fun"]))
+
